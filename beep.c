6e4396edeaa76eae9c7a1ec94f9c4c68b19e5a43
l3ka <34417308+l3ka@users.noreply.github.com>
2018-08-28 18:04:24 +0200

    Update beep.c

--- beep.c
+++ beep.c
@@ -38,20 +38,20 @@ static void play_sound(uint32_t nFrequence)
 {
 	uint32_t Div;
 	uint8_t tmp;
-	
+
 	// Set the PIT to the desired frequency
 	Div = 1193180 / nFrequence;
- 	outportb(0x43, 0xb6);
+	outportb(0x43, 0xb6);
 
- 	outportb(0x42, (uint8_t) (Div) );
- 	outportb(0x42, (uint8_t) (Div >> 8));
- 
+	outportb(0x42, (uint8_t) (Div) );
+	outportb(0x42, (uint8_t) (Div >> 8));
+
 	// And play the sound using the PC speaker
- 	tmp = inportb(0x61);
-  	if (tmp != (tmp | 3)) 
+	tmp = inportb(0x61);
+	if (tmp != (tmp | 3)) 
 	{
 		outportb(0x61, tmp | 3);
- 	}
+	}
 }
  
 // Make it shut up
